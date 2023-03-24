c975c9c30e48662a901f115359b11a4fcdbd88d4
KalongChan <82305211+KalongChan@users.noreply.github.com>
2023-03-24 18:48:26 +0800

    first commit

---
+++ src/components/Schedule.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+var convert = require("xml-js");
+
+const Schedule = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [schedule, setSchedule] = useState();
+
+  useEffect(() => {
+    if (!loaded) {
+      setLoaded(true);
+      return;
+    }
+    fetchData();
+  }, [loaded]);
+
+  const fetchData = async () => {
+    const res = await axios.get("http://ergast.com/api/f1/current");
+    var options = {compact: true, ignoreComment: true, spaces: 4};
+    const json = convert.xml2js(res.data, options);
+    const data = json.MRData.RaceTable.Race;
+    setSchedule(data);
+  };
+
+  console.log(schedule);
+  //           {race.Circuit.CircuitName._text}
+
+  return (
+    <div className="schedule">
+      {schedule?.map((race, index) => (
+        <div className="schedule__race" key={index}>
+          <div className="schedule__race-flag">
+            {race.Circuit.Location.Country._text}
+          </div>
+          <div className="schedule__race-info">
+            <div className="schedule__race-title">{race.RaceName._text}</div>
+            <div className="schedule__race-date">{race.Date._text}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Schedule;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Schedule from "@/components/Schedule";
+import {Fragment} from "react";
+
+const index = () => {
+  return (
+    <Fragment>
+      <Schedule />
+    </Fragment>
+  );
+};
+
+export default index;
