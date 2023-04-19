1eaf07eac2b9c4fd303cafa4d33c74473e23cc3c
KalongChan <82305211+KalongChan@users.noreply.github.com>
2023-04-19 17:54:08 +0800

    Added meta title, description, og image

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,18 @@
 import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.scss";
+import Head from "next/head";
 
 export default function App({Component, pageProps}) {
   return (
     <Layout>
+      <Head>
+        <title>F1 Result App</title>
+        <meta
+          name="description"
+          content="A web application for checking F1 results and standing"
+        />
+        <meta property="og:image" content="og.png" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
